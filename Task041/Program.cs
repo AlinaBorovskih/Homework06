268e658f807b76ca147c70cedd7a68c840f86b42
Alina_Borovskih <alinaberseneva@icloud.com>
2022-05-18 22:48:03 +0300

    Add task 41

---
+++ Task041/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите колличество чисел, которые вы собираетесь ввести:  ");
+int M = int.Parse(Console.ReadLine());
+int [] mass = new int [M];
+for (int i = 0; i < mass.Length; i++)
+{
+    Console.Write("Введите следующее число:  ");
+    mass[i] = int.Parse(Console.ReadLine());
+}
+
+int count = 0;
+
+for (int i = 0; i < mass.Length; i++)
+{
+    if (mass[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Колличество чисел больше нуля: {count}");

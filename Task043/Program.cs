4c092bc23ec02e13bf1dc5e11a0e39f1b27b755c
Alina_Borovskih <alinaberseneva@icloud.com>
2022-05-18 22:53:04 +0300

    Add task 43

---
+++ Task043/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
